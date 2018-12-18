e27499976debb00eeb13436b29a36a04146f55c9
yuma <glorymanchesteru@gmail.com>
2018-12-18 18:04:50 +0900

    skillページの作成

---
+++ pages/skill/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="home-page">
+    <section class="container">
+      <div>
+        <h1 class="title">
+          Skill
+        </h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 48px;
+  color: #07090a;
+  letter-spacing: 1px;
+}
+</style>
