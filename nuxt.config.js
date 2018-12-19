4ecd66785757b26a638d8166dde9076b8336127a
yuma <glorymanchesteru@gmail.com>
2018-12-19 21:38:15 +0900

    fontawesomeを導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,10 @@ module.exports = {
   ** Global CSS
   */
   css: [
+    '@fortawesome/fontawesome-free-webfonts',
+    '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css',
+    '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css',
+    '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css',
   ],
 
   /*
