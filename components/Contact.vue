f2a1bbfb0895b8632d2308293200a6de63fe57af
yuma <glorymanchesteru@gmail.com>
2018-12-22 14:36:10 +0900

    Contactページ作成

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,6 +4,33 @@
       <p>
         <strong class="title is-2">Contact</strong>
       </p>
+      <div>
+        <a
+          href="https://www.facebook.com/yuma.mitsuyoshi"
+          class="icon-margin">
+          <i class="fab fa-facebook-square fa-4x facebook-icon"></i>
+        </a>
+        <a
+          href="https://github.com/y-mitsuyoshi"
+          class="icon-margin">
+          <i class="fab fa-github-square fa-4x github-icon"></i>
+        </a>
+        <a
+          href="https://twitter.com/MitsuyoshiYuma"
+          class="icon-margin">
+          <i class="fab fa-twitter-square fa-4x twitter-icon"></i>
+        </a>
+      </div>
+    </div>
+    <div class="content has-text-centered">
+      <p>
+        <strong class="title is-2">Link</strong>
+      </p>
+      <div>
+        <strong class="title is-3">
+          <a href="http://tabilike.hatenablog.com">ブログ</a>
+        </strong>
+      </div>
     </div>
   </div>
 </template>
@@ -12,5 +39,22 @@
 .contact {
   background-color: rgb(253, 255, 231);
   background-size: cover;
+}
+
+.facebook-icon {
+  color: #305097;
+}
+
+.github-icon {
+  color: black;
+}
+
+.twitter-icon {
+  color: #55acee;
+}
+
+.icon-margin {
+  margin-left: 10px;
+  margin-right: 10px;
 }
 </style>
